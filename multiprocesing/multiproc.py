0b4245e0218175ab982fd006d07a250f7293ba9b
Terinor <kirstruk01@gmail.com>
2023-12-07 10:56:25 +0200

    multiprocesing list comprehension

--- multiprocesing/multiproc.py
+++ multiprocesing/multiproc.py
@@ -14,11 +14,7 @@ def timeit(func):
 
 def factorize(number):
     """Повертає список дільників числа."""
-    factors = []
-    for i in range(1, number + 1):
-        if number % i == 0:
-            factors.append(i)
-    return factors
+    return [i for i in range(1, number + 1) if number % i == 0]
 
 
 @timeit
